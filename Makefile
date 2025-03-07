b933729ae7e0d9d65c08afb75c75ab92f76bc376
Patrick ccc <patricksouth@users.noreply.github.com>
2025-03-07 10:09:19 +1000

    improvements to Makefile and deploy script.

--- Makefile
+++ Makefile
@@ -2,19 +2,18 @@ IMAGE_NAME=reflector-oidc
 TAG=1
 
 build:
-	docker build -t $(IMAGE_NAME):$(TAG) .
+        docker build -t $(IMAGE_NAME):$(TAG) .
 
-start:
-	./deploy_reflector_oidc.sh
+start: reflector.env
+        ./deploy_reflector_oidc.sh
 
 stop:
-	docker stack rm reflector-oidc
+        docker stack rm reflector-oidc
 
 restart:
-	docker stack rm reflector-oidc
-	sleep 5
-	./deploy_reflector_oidc.sh
+        docker stack rm reflector-oidc && \
+        sleep 5 && \
+        ./deploy_reflector_oidc.sh
 
 log:
-	docker service logs -f reflector-oidc_httpd
-
+        docker service logs -f reflector-oidc_httpd
--- deploy_reflector_oidc.sh
+++ deploy_reflector_oidc.sh
@@ -4,36 +4,58 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-# 16-Jan-2024
+# 7-Mar-2025
 
 cd "$(dirname "$0")"
-source reflector.env
+if [[ -e reflector.env ]]; then
+  source reflector.env
+else
+  echo
+  echo "       Missing reflector.env file"
+  echo "       Copy from reflector.env.default and update"
+  exit
+fi
 
 # This reflector can use certs/keys managed by a Traefik proxy.
-# Traefik retrieves and stores web SSL certs in the $LETSENCRYPT path the first time it runs and ensures they are current.
-# You can use your own certs by placing them in the ./letsencrypt/{certs,private} directories respectively, before starting this services.
-# Otherwise, let this script create the symlink to the ${LETSENCRYPT} location (specified in the reflector.env file).
+# Traefik retrieves and stores web SSL certs in the $LETSENCRYPT path
+# the first time it runs and ensures they are current.
+# You can use your own certs by placing them in the
+# ./letsencrypt/{certs,private} directories respectively,
+# before starting this service.
+#
+# Otherwise, let this script create the symlink to the ${LETSENCRYPT}
+# location (specified in the reflector.env file).
 # See README.md for details on the Traefik proxy.
 #
 
+if [[ -z ${APACHE_FQDN} ]]; then
+  echo
+  echo "       APACHE_FQDN environment variable not set in reflector.env!"
+  echo
+  exit
+fi
+
 if [[ -z ${LETSENCRYPT} ]]; then
   echo
-  echo "       LetsEncrypt environment variable not set!"
+  echo "       LETSENCRYPT environment variable not set in reflector.env!"
   echo
   exit
 fi
 
-if [[ ! -d ${LETSENCRYPT} ]]; then
+if [[ ! -d ${LETSENCRYPT} && ! -d ./letsencrypt ]]; then
   echo
-  echo "      LetsEncrypt target directory not available!"
+  echo "       LETSENCRYPT environment variable not set in reflector.env or ./letsencrypt target directory not available!"
+  echo
+  echo "      If not using a Traefik proxy, create the ./letsencrypt/{certs,private} "
+  echo "      directories and add certificates."
   echo
   exit
 fi
 
-if [[ ! -h ./letsencrypt ]]; then
+if [[ ! -h ./letsencrypt || ! -d ./letsencrypt ]]; then
   ln -s ${LETSENCRYPT} ./letsencrypt
   echo
-  echo "       Adding a symlink for LetsEncrypt."
+  echo "       Adding a symlink for LETSENCRYPT."
   echo
 fi
 
@@ -55,9 +77,9 @@ if [[ -e ./letsencrypt/acme.json ]]; then
   popd
 fi
 
-if [[ ! -e letsencrypt/certs/${APACHE_FQDN}.crt || ! -e letsencrypt/private/${APACHE_FQDN}.key ]];then
+if [[ ! -e ./letsencrypt/certs/${APACHE_FQDN}.crt || ! -e ./letsencrypt/private/${APACHE_FQDN}.key ]];then
   echo
-  echo "     Missing SSL cert or key files at 'letsencrypt/certs' OR 'letsencrypt/private' "
+  echo "     Missing SSL certificate or private key files at 'letsencrypt/certs' OR 'letsencrypt/private' "
   echo "     for the host ${APACHE_FQDN}."
   echo
   exit
@@ -65,7 +87,6 @@ fi
 
 if [ ! -d "logs" ]; then
   mkdir logs
-
 fi
 
 if [[ -n $(docker stack ls --format '{{.Name}}' | grep reflector-oidc) ]]; then
@@ -79,6 +100,11 @@ fi
 docker stack deploy --compose-file docker-compose.yml reflector-oidc
 
 echo
-echo "    Want to display logs, use: make log-reflector"
+echo "    To display logs, use: make log"
 echo
-echo "    Want to view OIDC logs use: tail -f logs/error.log"
+echo "    See additonal logs in: logs/error.log"
+echo 
+echo "    To stop the service, use: make stop"
+echo
+echo "    To restart the service, use: make restart"
+echo
